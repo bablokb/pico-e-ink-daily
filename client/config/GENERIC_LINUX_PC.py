f5dd6431c77df94a5be76b22c86839c03fd48f5a
Bernhard Bablok <bablokb@gmx.de>
2023-04-21 15:56:29 +0200

    support wifi

--- client/config/GENERIC_LINUX_PC.py
+++ client/config/GENERIC_LINUX_PC.py
@@ -12,6 +12,18 @@ import sys
 import board
 from blinka_displayio_pygamedisplay import PyGameDisplay
 from hwconfig import HWConfig
+import socket
+import adafruit_requests
+
+class WifiImpl:
+  """ request-implementation using sockets from CPython """
+  def connect(self):
+    self._http = adafruit_requests.Session(socket)
+  def get(self,url):
+    response = self._http.get(url)
+    result = response.json()
+    response.close()
+    return result
 
 class PygameConfig(HWConfig):
   """ GENERIC_LINUX_PC specific configuration-class """
@@ -24,6 +36,10 @@ class PygameConfig(HWConfig):
   def bat_level(self):
     """ return battery level """
     return 3.6
+
+  def wifi(self):
+    """ return wifi-interface """
+    return WifiImpl()
 
   def shutdown(self):
     """ leave program """
