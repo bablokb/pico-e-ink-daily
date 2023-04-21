5ee16331b744fc1d1b38cbabbf8df0cbdc475447
Bernhard Bablok <bablokb@gmx.de>
2023-04-21 15:57:16 +0200

    fetch data from server

--- client/main.py
+++ client/main.py
@@ -13,6 +13,64 @@
 
 # --- configuration constants   -------------------------------------------
 
+# test-data
+values = {
+  "day": "13",
+  "date": "Donnerstag 13.04.2023",
+  "now": "13.04.23 12:34",
+  "weekday": True,
+  "events": [
+     {
+       "start": "00:00",
+       "end": "23:59",
+       "summary": "Naturfototage",
+       "location": "",
+       "color": "green"
+     },
+     {
+       "start": "08:00",
+       "end": "09:00",
+       "summary": "Frühstück",
+       "location": "Küche",
+       "color": "green"
+     },
+     {
+       "start": "09:00",
+       "end": "10:00",
+       "summary": "Migrations-Check",
+       "location": "Meetingraum",
+       "color": "orange"
+     },
+     {
+       "start": "10:00",
+       "end": "11:00",
+       "summary": "Go-Live",
+       "location": "",
+       "color": "orange"
+     },
+     {
+       "start": "14:00",
+       "end": "16:00",
+       "summary": "Meeting",
+       "location": "Büro",
+       "color": "blue"
+     },
+     {
+       "start": "14:00",
+       "end": "15:00",
+       "summary": "Meeting2",
+       "location": "Büro",
+       "color": "red"
+     },
+     {
+       "start": "15:00",
+       "end": "16:00",
+       "summary": "Meeting3",
+       "location": "Büro",
+       "color": "yellow"
+     }
+   ]
+  }
 
 # --- imports   -----------------------------------------------------------
 
@@ -32,6 +90,7 @@ except:
   hw_impl = builtins.__import__(config_file,None,None,["config"],0)
   print("using default implementation")
 
+from config.secrets import secrets
 from agenda import Agenda
 
 # --- application class   ----------------------------------------------------
@@ -53,7 +112,7 @@ class App:
     
     self.display    = hw_impl.config.get_display()
     self._bat_level = hw_impl.config.bat_level
-    #self._wifi     = hw_impl.config.wifi
+    self._wifi      = hw_impl.config.wifi()
     self._rtc_ext   = hw_impl.config.get_rtc_ext()
     self._shutdown  = hw_impl.config.shutdown
 
@@ -63,11 +122,18 @@ class App:
     """ update rtc """
     pass
 
-  # --- fetch daily agenda   -------------------------------------------------
+  # --- update data from server   --------------------------------------------
 
-  def fetch_agenda(self):
-    """ fetch daily agenda from server """
-    pass
+  def update_data(self):
+    """ update data """
+
+    try:
+      self._wifi.connect()
+      self._data = self._wifi.get(secrets.url)
+    except:
+      raise
+      self._data = values
+    self._data["bat_level"] = self._bat_level()
 
   # --- update display   -----------------------------------------------------
 
@@ -75,7 +141,7 @@ class App:
     """ update display """
 
     agenda = Agenda(self.display)
-    self.display.show(agenda.get_content())
+    self.display.show(agenda.get_content(self._data))
     self.display.refresh()
     if hasattr(self.display,"time_to_refresh"):
       time.sleep(self.display.time_to_refresh)
@@ -93,6 +159,7 @@ print(f"startup: {time.monotonic()-start:f}s")
 
 # pygame test-environment
 if hasattr(app.display,"check_quit"):
+  app.update_data()
   app.update_display()
   while True:
     if app.display.check_quit():
@@ -103,7 +170,7 @@ if hasattr(app.display,"check_quit"):
 else:
   while True:
     start = time.monotonic()
-    app.fetch_agenda()
+    app.update_data()
     print(f"fetch_agenda: {time.monotonic()-start:f}s")
     app.update_display()
     print(f"update_display: {time.monotonic()-start:f}s")
