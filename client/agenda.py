2fbbe70ef23ee5abc6fc70b652bf4741ad9de5fb
Bernhard Bablok <bablokb@gmx.de>
2023-04-27 19:41:36 +0200

    refactored ui-settings to ui_settings.py

--- client/agenda.py
+++ client/agenda.py
@@ -18,43 +18,7 @@ from adafruit_display_text import label
 from adafruit_display_shapes.line import Line
 from adafruit_bitmap_font import bitmap_font
 
-palette = displayio.Palette(7)
-palette[0] = 0xFFFFFF
-palette[1] = 0x000000
-palette[2] = 0x0000FF
-palette[3] = 0x00FF00
-palette[4] = 0xFF0000
-palette[5] = 0xFFFF00
-palette[6] = 0xFFA500
-
-WHITE  = 0
-BLACK  = 1
-BLUE   = 2
-GREEN  = 3
-RED    = 4
-YELLOW = 5
-ORANGE = 6
-
-# map color-names to (BG_COLOR,FG_COLOR)
-colors = {
-  "white":  (WHITE,BLACK),
-  "black":  (BLACK,WHITE),
-  "blue":   (BLUE,WHITE),
-  "green":  (GREEN,BLACK),
-  "red":    (RED,BLACK),
-  "yellow": (YELLOW,BLACK),
-  "orange": (ORANGE,BLACK)
-  }
-
-ui_settings = {
-  "DAY_FONT":  "fonts/DejaVuSerif-Bold-60.bdf",
-  "DATE_FONT": "fonts/DejaVuSans-BoldOblique-35.bdf",
-  "TIME_FONT": "fonts/DejaVuSerif-BoldItalic-20.bdf",
-  "TEXT_FONT": "fonts/DejaVuSerif-20.bdf",
-  "STATUS_FONT": "fonts/DejaVuSerif-18.bdf",
-  "MARGIN":    5,
-  "PADDING":   3
-  }
+from ui_settings import UI_SETTINGS, COLORS, UI_COLOR_MAP, UI_PALETTE
 
 # --- Agenda Class for layout   ----------------------------------------------
 
@@ -66,11 +30,11 @@ class Agenda:
     """ constructor: create ressources """
 
     self._display     = display
-    self._time_font   = bitmap_font.load_font(ui_settings["TIME_FONT"])
-    self._text_font   = bitmap_font.load_font(ui_settings["TEXT_FONT"])
-    self._status_font = bitmap_font.load_font(ui_settings["STATUS_FONT"])
-    self._margin      = ui_settings["MARGIN"]
-    self._padding     = ui_settings["PADDING"]
+    self._time_font   = bitmap_font.load_font(UI_SETTINGS.TIME_FONT)
+    self._text_font   = bitmap_font.load_font(UI_SETTINGS.TEXT_FONT)
+    self._status_font = bitmap_font.load_font(UI_SETTINGS.STATUS_FONT)
+    self._margin      = UI_SETTINGS.MARGIN
+    self._padding     = UI_SETTINGS.PADDING
 
   # --- helper method for debugging   ----------------------------------------
 
@@ -86,18 +50,18 @@ class Agenda:
     day_box = displayio.Group()
 
     if self._data["weekday"]:
-      bg_color = BLACK
+      bg_color = COLORS.BLACK
     else:
-      bg_color = RED
-    day_font = bitmap_font.load_font(ui_settings["DAY_FONT"])
+      bg_color = COLORS.RED
+    day_font = bitmap_font.load_font(UI_SETTINGS.DAY_FONT)
     day = label.Label(day_font,text=self._data["day"],
-                      color=palette[WHITE],
-                      background_color=palette[bg_color],
+                      color=UI_PALETTE[COLORS.WHITE],
+                      background_color=UI_PALETTE[bg_color],
                       background_tight=True,
                       anchor_point=(0,0),
                       anchored_position=(self._margin,self._margin))
 
-    background = Rectangle(pixel_shader=palette,x=0,y=0,
+    background = Rectangle(pixel_shader=UI_PALETTE,x=0,y=0,
                            width=day.bounding_box[2]+2*self._margin,
                            height=day.bounding_box[3]+2*self._margin,
                            color_index=bg_color)
@@ -117,13 +81,13 @@ class Agenda:
     day_box.y = 0
     header.append(day_box)
 
-    sep = Line(0,h,self._display.width,h,color=palette[BLACK])
+    sep = Line(0,h,self._display.width,h,color=UI_PALETTE[COLORS.BLACK])
     header.append(sep)
 
-    date_font   = bitmap_font.load_font(ui_settings["DATE_FONT"])
+    date_font   = bitmap_font.load_font(UI_SETTINGS.DATE_FONT)
     date = label.Label(date_font,text=self._data["date"],
-                      color=palette[BLACK],
-                      background_color=palette[WHITE],
+                      color=UI_PALETTE[COLORS.BLACK],
+                      background_color=UI_PALETTE[COLORS.WHITE],
                       background_tight=True,
                       anchor_point=(0,1),
                        anchored_position=(self._margin,h-self._margin))
@@ -138,22 +102,22 @@ class Agenda:
     footer = displayio.Group()
     status = label.Label(self._status_font,
                          text=f"Updated: {self._data['now']}",
-                         color=palette[BLACK],
-                         background_color=palette[WHITE],
+                         color=UI_PALETTE[COLORS.BLACK],
+                         background_color=UI_PALETTE[COLORS.WHITE],
                          base_alignment=True,
                          anchor_point=(0,1),
                          anchored_position=(self._margin,
                                             self._display.height-self._margin))
-    color = palette[BLACK]
+    color = UI_PALETTE[COLORS.BLACK]
     if self._data['bat_level'] < 3.1:
-      color = RED
+      color = COLORS.RED
     elif self._data['bat_level'] < 3.3:
-      color = ORANGE
+      color = COLORS.ORANGE
 
     level = label.Label(self._status_font,
                         text=f"{self._data['bat_level']:0.1f}V",
                         color=color,
-                        background_color=palette[WHITE],
+                        background_color=UI_PALETTE[COLORS.WHITE],
                         base_alignment=True,
                         anchor_point=(1,1),
                         anchored_position=(self._display.width-self._margin,
@@ -163,7 +127,7 @@ class Agenda:
     status.anchor_point = (0,level.bounding_box[3]/status.bounding_box[3])
     sep = Line(0,self._display.height-h,
                self._display.width,self._display.height-h,
-               color=palette[BLACK])
+               color=UI_PALETTE[COLORS.BLACK])
 
     footer.append(status)
     footer.append(level)
@@ -180,13 +144,13 @@ class Agenda:
     text = label.Label(self._text_font,
                        text="Mg",
                        background_tight=True,
-                       color=palette[BLACK],
-                       background_color=palette[WHITE])
+                       color=UI_PALETTE[COLORS.BLACK],
+                       background_color=UI_PALETTE[COLORS.WHITE])
     ts = label.Label(self._time_font,
                        text="23:59",
                        background_tight=True,
-                       color=palette[BLACK],
-                       background_color=palette[WHITE])
+                       color=UI_PALETTE[COLORS.BLACK],
+                       background_color=UI_PALETTE[COLORS.WHITE])
     h_box      = 2*max(text.bounding_box[3],ts.bounding_box[3]) + 3*self._padding
     txt_offset = self._margin + ts.bounding_box[2] + self._margin
 
@@ -195,10 +159,10 @@ class Agenda:
     y = 0
     for event in self._data["events"]:
       entry = displayio.Group()
-      bg_color,color = colors[event["color"]]
+      bg_color,color = UI_COLOR_MAP[event["color"]]
 
       # create background for entry
-      background = Rectangle(pixel_shader=palette,x=self._margin,y=y,
+      background = Rectangle(pixel_shader=UI_PALETTE,x=self._margin,y=y,
                              width=self._display.width - 2*self._margin,
                              height=h_box,
                              color_index=bg_color)
@@ -209,16 +173,16 @@ class Agenda:
         ts  = label.Label(self._time_font,
                           text=event["start"],
                           background_tight=True,
-                          color=palette[color],
-                          background_color=palette[bg_color],
+                          color=UI_PALETTE[color],
+                          background_color=UI_PALETTE[bg_color],
                           anchor_point=(0,0.5),
                           anchored_position=(self._margin,y+int(0.3*h_box)))
         entry.append(ts)
         ts  = label.Label(self._time_font,
                           text=event["end"],
                           background_tight=True,
-                          color=palette[color],
-                          background_color=palette[bg_color],
+                          color=UI_PALETTE[color],
+                          background_color=UI_PALETTE[bg_color],
                           anchor_point=(0,0.5),
                           anchored_position=(self._margin,y+int(0.75*h_box)))
         entry.append(ts)
@@ -227,8 +191,8 @@ class Agenda:
       text = label.Label(self._text_font,
                          text=event["summary"],
                          background_tight=True,
-                         color=palette[color],
-                         background_color=palette[bg_color],
+                         color=UI_PALETTE[color],
+                         background_color=UI_PALETTE[bg_color],
                          anchor_point=(0,0.5),
                          anchored_position=(txt_offset,y+int(0.3*h_box)))
       entry.append(text)
@@ -236,8 +200,8 @@ class Agenda:
         text = label.Label(self._text_font,
                            text=event["location"],
                            background_tight=True,
-                           color=palette[color],
-                           background_color=palette[bg_color],
+                           color=UI_PALETTE[color],
+                           background_color=UI_PALETTE[bg_color],
                            anchor_point=(0,0.5),
                            anchored_position=(txt_offset,y+int(0.75*h_box)))
         entry.append(text)
@@ -257,10 +221,10 @@ class Agenda:
     self._data = data
     gc.collect()
     g = displayio.Group()
-    background = Rectangle(pixel_shader=palette,x=0,y=0,
+    background = Rectangle(pixel_shader=UI_PALETTE,x=0,y=0,
                            width=self._display.width,
                            height=self._display.height,
-                           color_index=WHITE)
+                           color_index=COLORS.WHITE)
     g.append(background)
 
     (header,h) = self._get_header()
